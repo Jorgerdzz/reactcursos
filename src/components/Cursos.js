314af7c4d5523e1af25d39010dbdc27b81f31817
Jorgerdzz <jorgerodriguezalonso20@gmail.com>
2025-10-28 18:37:01 +0100

    Init proyecto cursos

---
+++ src/components/Cursos.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Cursos extends Component {
+  render() {
+    return (
+      <div>
+        <h1>PRÁCTICA REACT CURSOS</h1>
+        <select>
+            
+        </select>
+      </div>
+    )
+  }
+}
